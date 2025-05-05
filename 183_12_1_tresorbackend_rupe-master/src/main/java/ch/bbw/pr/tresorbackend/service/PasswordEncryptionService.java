dcad8dff7757d30d5c06087dcc2ed6a95376a60f
pirnet7 <brunovarrese06@gmail.com>
2025-05-05 20:21:23 +0200

    add verifyPassword and hashPassword function

--- 183_12_1_tresorbackend_rupe-master/src/main/java/ch/bbw/pr/tresorbackend/service/PasswordEncryptionService.java
+++ 183_12_1_tresorbackend_rupe-master/src/main/java/ch/bbw/pr/tresorbackend/service/PasswordEncryptionService.java
@@ -2,10 +2,11 @@ package ch.bbw.pr.tresorbackend.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import java.security.SecureRandom;
+
 import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.bcrypt.BCrypt;
-import java.util.Base64;
 
+import java.util.Base64;
 /**
  * PasswordEncryptionService
  * @author Peter Rutschmann
@@ -14,15 +15,27 @@ import java.util.Base64;
 public class PasswordEncryptionService {
    @Value("${password.pepper}")
    private String pepper;
-
    private final SecureRandom secureRandom;
 
    public PasswordEncryptionService() {
-      //todo anpassen!
+      this.secureRandom = new SecureRandom();
    }
 
+   /**
+    * Hashe das Passwort mit BCrypt und "salt" und "pepper"
+    */
    public String hashPassword(String password) {
-      //todo anpassen!
-      return password;
+      String pepperedPassword = password + pepper;
+      String salt = BCrypt.gensalt(12);
+      return BCrypt.hashpw(pepperedPassword, salt);
+   }
+
+   /**
+    * Verifiziere das Passwort gegen ein Hash
+    */
+   public boolean verifyPassword(String password, String hashedPassword) {
+      String pepperedPassword = password + pepper;
+
+      return BCrypt.checkpw(pepperedPassword, hashedPassword);
    }
 }
